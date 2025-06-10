d0f902b0ba496869d8561241ab0362c6b2d4c0b2
qiwenjjin <49423196+qiwenjjin@users.noreply.github.com>
2025-06-10 16:13:02 +0800

    Update main.m

--- evaluation/main.m
+++ evaluation/main.m
@@ -4,20 +4,12 @@ close all
 
 N = 1;
 value_mat = zeros([N * 2,10]);
-% Refference
-% pan_path = '/home/pcl/BaiXu/MyNet/FusionNN/GF-2/Contrast/GPPNN20223172132/TestFolder/pan/'
-% pan_label_path ='/home/pcl/BaiXu/MyNet/FusionNN/GF-2/Contrast/GPPNN20223172132/TestFolder/pan_label/'
-% ms_path = '/home/pcl/BaiXu/MyNet/FusionNN/GF-2/Contrast/GPPNN20223172132/TestFolder/ms/'
-% ms_label_path = '/home/pcl/BaiXu/MyNet/FusionNN/GF-2/Contrast/GPPNN20223172132/TestFolder/ms_label/'
-% 
-% lr_fusion_path = '/home/pcl/BaiXu/MyNet/FusionNN/GF-2/Contrast/GPPNN20223172132/FusionFolderLR/'
-% hr_fusion_path = '/home/pcl/BaiXu/MyNet/FusionNN/GF-2/Contrast/GPPNN20223172132/FusionFolderHR/'
 
-pan_label_path = '../test_GF2/pan/';
-ms_label_path = '../test_GF2/gt/';
+pan_label_path = '../test_QB2/pan/';
+ms_label_path = '../test_QB2/gt/';
 
-lr_fusion_path = '../test_GF2/result/';
-hr_fusion_path = '../test_GF2/bic/';
+lr_fusion_path = '../test_QB2/result/';
+hr_fusion_path = '../test_QB2/bic/';
 
 value =  Geteval(ms_label_path,lr_fusion_path,pan_label_path,hr_fusion_path,lr_fusion_path,200,'none');
 value_mat(i*2 + 1:i*2 + 2,:) = value;     
